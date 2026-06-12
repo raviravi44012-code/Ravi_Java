dca586d028a448b0d1cfff8696bc09da349f9328
raviravi44012-code <raviravi44012@gmail.com>
2026-06-12 17:30:39 +0530

    Create Ravi.java

---
+++ Ravi.java
@@ -0,0 +1,20 @@
+package ravi;
+import java.util.Scanner;
+public class Ravi
+{
+ public static void main(String args[])
+ {
+	   Scanner ravi=new Scanner(System.in);
+	   System.out.println("Enter the 2 numbers...");
+	   int o=ravi.nextInt();
+	   int r=ravi.nextInt();
+	   int a=o;
+	       o=r;
+	       r=a;
+	      System.out.println(o);
+	       System.out.println(r);
+
+ }
+}
+
+ 
